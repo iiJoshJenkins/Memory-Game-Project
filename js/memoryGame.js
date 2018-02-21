d108333a76ffa19439a598afa56ae59126593838
iiJoshJenkins <joshua.jenkins@live.co.uk>
2018-02-21 22:42:08 +0000

    Fixed a few things : - Made the createMenu function use a menu object as a template.
    - Moved the click event listener to the createMenu function as there's
    no reason to have an event listener if there's no options.
    
    - Adding an event handler function to clicking on the menus (menuClick)
    which dynically calls whatever option was clicked.
    
    - Fixed typos and generally just cleaned up code.

--- js/memoryGame.js
+++ js/memoryGame.js
@@ -1,29 +1,51 @@
+const gameContainer = document.getElementById('gameContainer');
+const difficultyMenu = {
+    title: 'Difficulty',
+    options : {
+        'Easy' : createGame(9),
+        'Normal' : createGame(16),
+        'Hard' : createGame(20)
+    }
+}
+const mainMenu = {
+    title: 'Memory Game',
+    options : {
+        'Play' : createMenu(difficultyMenu),
+        'Instructions' : 'instructions.html'
+    }
+}
+
 // Trying to dynamically create menus.
 // Will return the DOM object needed to append straight to the body.
-function createMenu(title, ...menuOptions){
+function createMenu(menuTemplate){
+    // Creating the container for the menu
     const menuContainer = document.createElement('div');
     menuContainer.setAttribute('class', 'menuContainer');
-
+    // Creating the title for the menu
     const menuTitle = document.createElement('h1');
     menuTitle.setAttribute('class', 'menuTitle');
-    menuTitle.innerHTML = `<strong> ${title} </strong>`;
+    menuTitle.innerHTML = `<strong> ${menuTemplate.title} </strong>`;
     menuContainer.appendChild(menuTitle);
-
-    if(menuOptions.length > 0){
+    // Checking if the amount of items inside menuOptions is greater than one
+    // Using rest parameter so it could and in some cases it should
+    if(menuTemplate.options){
+        // Creating the list for the option to go in
         const menuOptionsList = document.createElement('ul');
         menuOptionsList.setAttribute('class', 'menuOptionsList');
-        for(let i = 0; i < menuOptions.length; i++){
+        // Looping through the array and adding all the options to the list
+        for(let option in menuTemplate.options){
             const optionsItem = document.createElement('li');
             const itemTitle = document.createElement('h3');
-            itemTitle.innerHTML = menuOptions[i];
+            itemTitle.innerHTML = option;
 
             optionsItem.appendChild(itemTitle);
             menuOptionsList.appendChild(optionsItem);
         }
-
+        // Creating the eventListeners for the options menu
+        // Doing this to allow hover effects and other things like that.
         menuOptionsList.addEventListener('mouseover', e => hoverOver(e, 'in'));
         menuOptionsList.addEventListener('mouseout', e => hoverOver(e, 'out'));
-
+        menuOptionsList.addEventListener('click', e => menuClick(e, menuTemplate.options));
         menuContainer.appendChild(menuOptionsList);
     }
 
@@ -31,61 +53,38 @@ function createMenu(title, ...menuOptions){
 }
 
 function createGame(amountOfCards){
-    const gameMenu = createMenu("Memory Game");
-    gameContainer.appendChild(gameMenu);
+    // Needs to return a dom element, for now just adding a header
+    const gameHeader = document.createElement('h1');
+    gameHeader.innerHTML = `Game created with ${amountOfCards} cards!`
+
+    return gameHeader;
 }
 
-const startMenu = createMenu("Memory Game", "Play", "Instructions");
-
 document.addEventListener('DOMContentLoaded', e => {
-    const gameContainer = document.getElementById('gameContainer');
-    document.getElementById('gameContainer').appendChild(startMenu);
-
-    startMenu.addEventListener('click', e => {
-        const target = e.target,
-            nodeName = target.nodeName.toLowerCase();
-        let option;
-        if(nodeName == 'li' || nodeName == 'h3'){
-            option = nodeName == 'li' ? target.firstElementChild.innerText : target.innerText;
-            gameContainer.innerHTML = '';
-            if(option == 'Play'){
-                const difficultyMenu = createMenu("Difficulty", "Easy", "Normal", "Hard");
-                gameContainer.appendChild(difficultyMenu);
-                difficultyMenu.addEventListener('click', e => {
-                    const target = e.target,
-                          nodeName = target.nodeName.toLowerCase();
-                    let option;
-                    if(nodeName == 'li' || nodeName == 'h3'){
-                        option = nodeName == 'li' ? target.firstElementChild.innerText : target.innerText;
-                        gameContainer.innerHTML = '';
-                        console.log(option);
-                        switch(option){
-                            case 'Easy':
-                                createGame(9);
-                                break;
-                            case 'Normal':
-                                createGame(16);
-                                break;
-                            case 'Hard':
-                                createGame(20);
-                                break;
-                            default:
-                                console.log("ERROR: Somehow an option wasn't pressed");
-                        }
-                    }
-                });
-            }
-            else if(option == 'Instructions'){
-                window.location.href = 'instructions.html';
-            }
-        }
-    });
+    gameContainer.appendChild(createMenu(mainMenu));
 });
-
 
 /*
     MISC FUNCTIONS
 */
+
+// This is the event handler for clicking a button in the options
+function menuClick(event, menuOptions){
+    gameContainer.innerHTML = '';
+    let target = event.target,
+          nodeName = target.nodeName.toLowerCase();
+    if(nodeName == 'li' || nodeName == 'h3'){
+        if(nodeName == 'li'){
+            target = target.firstChild;
+        }
+        if(menuOptions['Instructions'] && target.innerHTML == 'Instructions'){
+            window.location.href = menuOptions['Instructions'];
+        }
+        else{
+            gameContainer.appendChild(menuOptions[target.innerHTML]);
+        }
+    }
+}
 
 // This is the event handler for hovering over the menu options
 // It checks if you're hovering over and li or h3 inside a list
@@ -96,8 +95,9 @@ function hoverOver(event, inOrOut){
     if(nodeName == 'li' || nodeName == 'h3'){
         if(inOrOut == 'in'){
             if(!target.classList.contains('hovered')){
-                if(nodeName == 'h3') target = target.parentElement;
-                    target.classList.add('hovered');
+                if(nodeName == 'h3')
+                    target = target.parentElement;
+                target.classList.add('hovered');
             }
             else return;
         }
